c4824a01df3faed872310a559d934d9621952057
Yejin-Han <yain3333@gmail.com>
2022-12-21 11:20:48 +0900

    music list create, timeupdate, loadeddata

---
+++ js/music_list.js
@@ -0,0 +1,39 @@
+//music 객체 배열로 지정
+const musicList=[
+	{
+		name: 'Alone',
+		artist: 'Color Out',
+		img: 'album1',
+		audio: 'Alone',
+	},
+	{
+		name: 'Mahidevran - Maze of sorrow',
+		artist: 'Mahidevran rock band',
+		img: 'album2',
+		audio: 'Mahidevran-Maze_of_sorrow',
+	},
+	{
+		name: 'No Rest Or Endless Rest',
+		artist: 'Lisofv',
+		img: 'album3',
+		audio: 'No_Rest_Or_Endless_Rest',
+	},
+	{
+		name: 'Old Melody',
+		artist: 'TAB',
+		img: 'album4',
+		audio: 'Old_Money',
+	},
+	{
+		name: 'Tantalizing Youth',
+		artist: 'Social Square',
+		img: 'album5',
+		audio: 'Tantalizing_Youth',
+	},
+	{
+		name: 'The Deep',
+		artist: 'Anitek',
+		img: 'album6',
+		audio: 'The_Deep',
+	}
+]
--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
 const musicWrap=document.querySelector('.wrapper');
 const musicAudio=musicWrap.querySelector('#main-audio');
 const playBtn=musicWrap.querySelector('#play-btn');
+const progress=musicWrap.querySelector('.m-progress');
+const progressBar=progress.querySelector('.bar');
+const playTime=progress.querySelector('.current');
+const totTime=progress.querySelector('.duration');
 const playMusic=()=>{
 	playBtn.innerHTML="pause";
+	musicAudio.play();
 }
 const pauseMusic=()=>{
 	playBtn.innerHTML="play_arrow";
+	musicAudio.pause();
 }
 playBtn.addEventListener('click',()=>{
 	let getText=playBtn.innerText;
 	(getText=="pause")? pauseMusic() : playMusic();
+});
+let list_index=0; //musicList[0] ~ musicList[5] 순환 호출
+musicAudio.addEventListener('timeupdate',(e)=>{
+	let curr=e.target.currentTime;
+	let duration=e.target.duration;
+	let progressRatio=(curr/duration)*100;
+	progressBar.style.width=`${progressRatio}%`;
+	//재생시간 표시
+	let currMin=Math.floor(curr/60);
+	let currSec=Math.floor(curr%60);
+	if(currSec<10) currSec=`0${currSec}`;
+	playTime.innerHTML=`${currMin}:${currSec}`;
+	musicAudio.addEventListener('loadeddata',()=>{
+		//전체시간 표시
+		let totDuration=musicAudio.duration;
+		let totMin=Math.floor(totDuration/60);
+		let totSec=Math.floor(totDuration%60);
+		if(totSec<10) totSec=`0${totSec}`; //1~9초는 01~09초로 표기
+		totTime.innerHTML=`${totMin}:${totSec}`;
+	});
 });
